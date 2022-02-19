f852de1cd650ab1784f8a529ed8b8c24c8c5f958
sujal18ss <95114364+sujal18ss@users.noreply.github.com>
2022-02-20 01:28:18 +0530

    Linux commands Updated

--- Saaransh.py
+++ Saaransh.py
@@ -1,11 +1,16 @@
 import os
+
+
 print("\n\t\t\t\t\b\b\b-----!! Saaransh !!-----\n")
+
+
 while True:
 	print(""" 
-	Option 01:Read About Sessions 
+	Option 01: Read About Sessions 
 	Option 02: Learn Linux Commands
 	Option 03: Bonus Command
-	Option 04: Exit
+	Option 04: Run some Linux commands
+	Option 05: Exit
 	""")
 
 	choice = int(input("Enter Your Choice: "))
@@ -83,8 +88,56 @@ while True:
 	elif choice == 3:
 		bonus_command = os.system("cat bonus_command.txt")
 	elif choice == 4:
+		while True:
+			print("""Which command do you wanna run\nPlease Enter your option...\n
+			Option-01: Show current date
+			Option-02: show current month's Calender		
+			Option-03: If you want continuously time
+			Option-04: To check my IP
+			Option-05: To open new Terminal
+			Option-06: Open firefox
+			Option-07: check user
+			Option-08: Create File
+			Option-09: Create Folder
+			Option- 0: Exit
+			""")
+			choice = int(input("Enter your choice: "))
+			if choice == 1:
+				date = os.system("date")
+				print(date)
+			elif choice == 2:
+				cal = os.system("cal")
+				print(cal)
+			elif choice == 3:
+				ip = os.system("bash garuda")
+				print(ip)
+			elif choice == 4:
+				ip = os.system("ifconfig")
+				print(ip)
+			elif choice == 5:
+				terminal = os.system("gnome-terminal")
+				print(terminal)
+			elif choice == 6:
+				browser = os.system("firefox")
+				print(browser)
+			elif choice == 7:
+				user = os.system("whoami")
+				print(user)
+			elif choice == 8:
+				create_file =os.system("gedit NewFile")
+			elif choice == 9:
+				create_folder =os.system("mkdir NewFolder")
+			elif choice == 0:
+				exit = os.system("exit")
+				print(exit)
+				print("Exiting Your Linux Commands ...")
+				break	
+			else :
+				print(" Invalid Option ....\n Please Try Again!!\n")
+	elif choice == 5:
 		exit = os.system("exit")
 		print("Exiting Your Entire Program ...")
 		break
+	
 	else :
 		print(" Invalid Option ....\n Please Try Again!!\n")
